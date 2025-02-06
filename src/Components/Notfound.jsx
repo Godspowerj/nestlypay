0748a00f81d4f89d438abf7a8c85b5cdbc0f1031
godspower <godspjona@gmail.com>
2025-02-06 10:43:44 +0100

    just made some changes

--- src/Components/Notfound.jsx
+++ src/Components/Notfound.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 
 const Notfound = () => {
   return (
-    <div className="flex flex-col w-full items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col w-full items-center text-center px-5 justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-      <p className="text-lg mb-4">Sorry, the page you are looking for does not exist.</p>
+      <p className="text-lg mb-4 ">Sorry, the page you are looking for does not exist.</p>
       <Link to="/" className="text-blue-500 hover:underline">
         Go back to the homepage
       </Link>
